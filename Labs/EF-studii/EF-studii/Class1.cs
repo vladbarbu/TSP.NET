050adee953b34d66e53281d6c1ed99bb489802bf
Vlad Barbu <vladbarbu14@yahoo.com>
2020-03-23 14:08:47 +0200

    EF-studii bad

---
+++ Labs/EF-studii/EF-studii/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_studii
+{
+    public class SelfReference:
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SelfReferenceId { get; private set; }
+        public string Name { get; set; }
+        public int? ParentSelfReferenceId { get; private set; }
+        [ForeignKey("ParentSelfReferenceId")]
+        public SelfReference ParentSelfReference { get; set; }
+        public virtual ICollection<SelfReference> References { get; set; }
+        public SelfReference()
+        {
+            References = new HashSet<SelfReference>();
+        }
+    }
+}
