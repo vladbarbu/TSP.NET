6a57f5d55b8f79dd153b66fafb65d786f661d380
Vlad <vladbarbu14@yahoo.com>
2020-04-28 15:08:20 +0300

    WCF si EF

---
+++ Labs/gRPC si EF/gRPC si EF/Services/GreeterService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace gRPC_si_EF
+{
+    public class GreeterService : gRPC_si_EF.gRPC_si_EFBase
+    {
+       
+    }
+}
