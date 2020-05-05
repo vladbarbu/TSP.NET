b6b2e2a18dd1d15d65353127d9ebcf8e05e31d8d
Vlad <vladbarbu14@yahoo.com>
2020-05-05 13:46:45 +0300

    Asp_Barbu_Vlad_Rp

---
+++ Labs/Asp_Barbu_Vlad_Rp/Asp_Barbu_Vlad_Rp/Data/Asp_Barbu_Vlad_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Barbu_Vlad_Rp.Models;
+
+namespace Asp_Barbu_Vlad_Rp.Data
+{
+    public class Asp_Barbu_Vlad_RpContext : DbContext
+    {
+        public Asp_Barbu_Vlad_RpContext (DbContextOptions<Asp_Barbu_Vlad_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Barbu_Vlad_Rp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Labs/Asp_Barbu_Vlad_Rp/Asp_Barbu_Vlad_Rp/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Asp_Barbu_Vlad_Rp.Data;
+using Asp_Barbu_Vlad_Rp.Models;
+
+namespace Asp_Barbu_Vlad_Rp.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Asp_Barbu_Vlad_Rp.Data.Asp_Barbu_Vlad_RpContext _context;
+
+        public DetailsModel(Asp_Barbu_Vlad_Rp.Data.Asp_Barbu_Vlad_RpContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
